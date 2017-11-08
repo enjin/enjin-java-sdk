bcbeab5e43922f3dfa006b7528a982adc84aece3
Damien Gallagher <damien.gallagher@gmail.com>
2017-11-08 14:01:47 +0000

    Added tests for the base action

---
+++ base/src/test/java/com/enjin/coin/sdk/BaseActionTest.java
@@ -0,0 +1,104 @@
+package com.enjin.coin.sdk;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BaseActionTest {
+
+	BaseAction baseAction;
+	
+	@Before
+	public void setUp() {
+		baseAction = new Identities();
+	}
+	
+	@Test
+	public void testContructor1() {
+		baseAction = new Identities();
+		assertNotNull(baseAction);
+		assertNotNull(baseAction.toString());
+	}
+	
+	@Test
+	public void testContructor2() {
+		baseAction = new Identities("http://localhost:8080");
+		assertNotNull(baseAction);
+		assertNotNull(baseAction.toString());
+	}
+	
+	@Test
+	public void testGetIdentitiesUrl1() {
+		String identitiesURL = baseAction.getIdentitiesUrl();
+		assertNotNull(identitiesURL);
+	}
+	
+	@Test
+	public void testGetIdentitiesUrl2() {
+		baseAction.setTrustedPlatformUrl("http://localhost:8081");
+		String identitiesURL = baseAction.getIdentitiesUrl();
+		assertNotNull(identitiesURL);
+	}
+	
+	@Test
+	public void testGetIdentitiesUrl3() {
+		baseAction.setTrustedPlatformUrl("http://localhost:8081/");
+		String identitiesURL = baseAction.getIdentitiesUrl();
+		assertNotNull(identitiesURL);
+	}
+	
+	@Test
+	public void testGetTokensUrl1() {
+		String tokensURL = baseAction.getTokensUrl();
+		assertNotNull(tokensURL);
+	}
+	
+	@Test
+	public void testGetTokensUrl2() {
+		baseAction.setTrustedPlatformUrl("http://localhost:8081");
+		String tokensURL = baseAction.getTokensUrl();
+		assertNotNull(tokensURL);
+	}
+	
+	@Test
+	public void testGetTokensUrl3() {
+		baseAction.setTrustedPlatformUrl("http://localhost:8081/");
+		String tokensURL = baseAction.getTokensUrl();
+		assertNotNull(tokensURL);
+	}
+	
+	@Test
+	public void testGetTransactionRequestsUrl1() {
+		String transactionRequestsURL = baseAction.getTransactionRequestsUrl();
+		assertNotNull(transactionRequestsURL);
+	}
+	
+	@Test
+	public void testGetTransactionRequestsUrl2() {
+		baseAction.setTrustedPlatformUrl("http://localhost:8081");
+		String transactionRequestsURL = baseAction.getTransactionRequestsUrl();
+		assertNotNull(transactionRequestsURL);
+	}
+	
+	@Test
+	public void testGetTransactionRequestsUrl3() {
+		baseAction.setTrustedPlatformUrl("http://localhost:8081/");
+		String transactionRequestsURL = baseAction.getTransactionRequestsUrl();
+		assertNotNull(transactionRequestsURL);
+	}
+	
+	@Test
+	public void testGetTrustedPlatformUrl_Null( ) {
+		String trustedPlatformURL = baseAction.getTrustedPlatformUrl();
+		assertNull(trustedPlatformURL);
+	}
+	@Test
+	public void testGetTrustedPlatformUrl_Success( ) {
+		baseAction.setTrustedPlatformUrl("http://localhost:8081/");
+		String trustedPlatformURL = baseAction.getTrustedPlatformUrl();
+		assertNotNull(trustedPlatformURL);
+	}
+	
+}
