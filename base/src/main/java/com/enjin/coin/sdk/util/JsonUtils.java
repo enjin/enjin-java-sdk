0063c4f278d659bc49642bc73863cae0d7709c74
Damien Gallagher <damien.gallagher@gmail.com>
2017-11-17 09:10:53 +0000

    Updated checkstyle errors for the JsonUtils class

--- base/src/main/java/com/enjin/coin/sdk/util/JsonUtils.java
+++ base/src/main/java/com/enjin/coin/sdk/util/JsonUtils.java
@@ -10,135 +10,160 @@ import java.io.FileReader;
 import java.util.logging.Logger;
 
 /**
- * <p>All utility methods for working with json</p>
+ * <p>
+ * All utility methods for working with json.
+ * </p>
  */
 public final class JsonUtils {
 
-    private static final Logger LOGGER = Logger.getLogger(JsonUtils.class.getName());
-
     /**
-     * Method to convert a json string to an object
-     *
-     * @param gson          - gson object
-     * @param jsonString    - json to convert to an object
-     * @param responseClass - class type to convert to
-     * @return An object representing the json in the fileReader
-     */
-    public static Object convertJsonToObject(Gson gson, String jsonString, Class<?> responseClass) {
-        Object responseObject = null;
+	 * Logger variable.
+	 */
+	private static final Logger LOGGER = Logger.getLogger(JsonUtils.class.getName());
 
-        if (gson == null || StringUtils.isEmpty(jsonString) || ObjectUtils.isNull(responseClass)) {
-            LOGGER.warning("gson passed in is null or jsonString passed in is null or empty or the responseClass is null");
-            return responseObject;
-        }
+	/**
+	 * Class constructor.
+	 */
+	protected JsonUtils() {
 
-        //JSON from file to Object
-        try {
-            LOGGER.fine(String.format("jsonString:%s", jsonString));
-            responseObject = gson.fromJson(jsonString, responseClass);
-        } catch (JsonSyntaxException e) {
-            LOGGER.warning(String.format("A JsonSyntaxException has occured. Exception: %s", StringUtils.exceptionToString(e)));
-        }
+	}
 
-        return responseObject;
-    }
+	/**
+	 * Method to convert a json string to an object.
+	 *
+	 * @param gson
+	 *            gson object
+	 * @param jsonString
+	 *            json to convert to an object
+	 * @param responseClass
+	 *            class type to convert to
+	 * @return An object representing the json in the fileReader
+	 */
+	public static Object convertJsonToObject(final Gson gson, final String jsonString, final Class<?> responseClass) {
+		Object responseObject = null;
 
-    /**
-     * Method to convert a json string from a file to an object
-     *
-     * @param gson          - gson object
-     * @param filePath      - filePath to read json from
-     * @param responseClass - class type to convert to
-     * @return An object representing the json at the filePath
-     */
-    public static Object convertJsonFromFileToObject(Gson gson, String filePath, Class<?> responseClass) {
-        Object responseObject = null;
+		if (gson == null || StringUtils.isEmpty(jsonString) || ObjectUtils.isNull(responseClass)) {
+			LOGGER.warning("gson passed in is null or jsonString passed in is null or empty or the responseClass is null");
+			return responseObject;
+		}
 
-        if (gson == null || StringUtils.isEmpty(filePath) || ObjectUtils.isNull(responseClass)) {
-            LOGGER.warning("gson passed in is null or filePath passed in is null or empty or the responseClass is null");
-            return responseObject;
-        }
+		// JSON from file to Object
+		try {
+			LOGGER.fine(String.format("jsonString:%s", jsonString));
+			responseObject = gson.fromJson(jsonString, responseClass);
+		} catch (JsonSyntaxException e) {
+			LOGGER.warning(String.format("A JsonSyntaxException has occured. Exception: %s", StringUtils.exceptionToString(e)));
+		}
 
-        //JSON from file to Object
-        try {
-            LOGGER.fine(String.format("filePath:%s", filePath));
-            FileReader fileReader = new FileReader("jsonFile.json");
-            JsonReader jsonReader = new JsonReader(fileReader);
-            responseObject = gson.fromJson(jsonReader, responseClass);
-        } catch (JsonSyntaxException e) {
-            LOGGER.warning(String.format("A JsonSyntaxException has occured. Exception: %s", StringUtils.exceptionToString(e)));
-        } catch (FileNotFoundException e) {
-            LOGGER.warning(String.format("A FileNotFoundException has occured. Exception: %s", StringUtils.exceptionToString(e)));
-        }
+		return responseObject;
+	}
 
-        return responseObject;
-    }
+	/**
+	 * Method to convert a json string from a file to an object.
+	 *
+	 * @param gson
+	 *            gson object
+	 * @param filePath
+	 *            filePath to read json from
+	 * @param responseClass
+	 *            class type to convert to
+	 * @return An object representing the json at the filePath
+	 */
+	public static Object convertJsonFromFileToObject(final Gson gson, final String filePath, final Class<?> responseClass) {
+		Object responseObject = null;
 
-    /**
-     * Method to convert json from a fileReader to an object
-     *
-     * @param gson          - gson object
-     * @param fileReader    - fileReader with the required content
-     * @param responseClass - class type to convert to
-     * @return An object representing the json in the fileReader
-     */
-    public static Object convertJsonFromFileReaderToObject(Gson gson, FileReader fileReader, Class<?> responseClass) {
-        Object responseObject = null;
+		if (gson == null || StringUtils.isEmpty(filePath) || ObjectUtils.isNull(responseClass)) {
+			LOGGER.warning("gson passed in is null or filePath passed in is null or empty or the responseClass is null");
+			return responseObject;
+		}
 
-        if (gson == null || fileReader == null || ObjectUtils.isNull(responseClass)) {
-            LOGGER.warning("gson passed in is null or fileReader passed in is null or empty or the responseClass is null");
-            return responseObject;
-        }
+		// JSON from file to Object
+		try {
+			LOGGER.fine(String.format("filePath:%s", filePath));
+			FileReader fileReader = new FileReader("jsonFile.json");
+			JsonReader jsonReader = new JsonReader(fileReader);
+			responseObject = gson.fromJson(jsonReader, responseClass);
+		} catch (JsonSyntaxException e) {
+			LOGGER.warning(String.format("A JsonSyntaxException has occured. Exception: %s", StringUtils.exceptionToString(e)));
+		} catch (FileNotFoundException e) {
+			LOGGER.warning(String.format("A FileNotFoundException has occured. Exception: %s", StringUtils.exceptionToString(e)));
+		}
 
-        //JSON from file to Object
-        try {
-            JsonReader jsonReader = new JsonReader(fileReader);
-            responseObject = gson.fromJson(jsonReader, responseClass);
-        } catch (JsonSyntaxException e) {
-            LOGGER.warning(String.format("A JsonSyntaxException has occured. Exception: %s", StringUtils.exceptionToString(e)));
-        }
+		return responseObject;
+	}
 
-        return responseObject;
-    }
+	/**
+	 * Method to convert json from a fileReader to an object.
+	 *
+	 * @param gson
+	 *            gson object
+	 * @param fileReader
+	 *            fileReader with the required content
+	 * @param responseClass
+	 *            class type to convert to
+	 * @return An object representing the json in the fileReader
+	 */
+	public static Object convertJsonFromFileReaderToObject(final Gson gson, final FileReader fileReader, final Class<?> responseClass) {
+		Object responseObject = null;
 
-    /**
-     * Method to convert an object to a json string
-     *
-     * @param gson       - gson object
-     * @param jsonObject - jsonObject tp cpmvert
-     * @return - A string with the object in json format
-     */
-    public static String convertObjectToJson(Gson gson, Object jsonObject) {
-        String jsonString = null;
+		if (gson == null || fileReader == null || ObjectUtils.isNull(responseClass)) {
+			LOGGER.warning("gson passed in is null or fileReader passed in is null or empty or the responseClass is null");
+			return responseObject;
+		}
 
-        if (gson == null || ObjectUtils.isNull(jsonObject)) {
-            LOGGER.warning("gson or jsonObject passed in is null");
-            return jsonString;
-        }
+		// JSON from file to Object
+		try {
+			JsonReader jsonReader = new JsonReader(fileReader);
+			responseObject = gson.fromJson(jsonReader, responseClass);
+		} catch (JsonSyntaxException e) {
+			LOGGER.warning(String.format("A JsonSyntaxException has occured. Exception: %s", StringUtils.exceptionToString(e)));
+		}
 
-        jsonString = gson.toJson(jsonObject);
-        LOGGER.fine(String.format("jsonString:%s", jsonString));
-        return jsonString;
-    }
+		return responseObject;
+	}
 
-    /**
-     * Method to convert an object to a json tree
-     *
-     * @param gson       - gson object
-     * @param jsonObject - jsonObject to convert
-     * @return a JsonElement
-     */
-    public static JsonElement convertObjectToJsonTree(Gson gson, Object jsonObject) {
-        JsonElement jsonElement = null;
+	/**
+	 * Method to convert an object to a json string.
+	 *
+	 * @param gson
+	 *            gson object
+	 * @param jsonObject
+	 *            jsonObject tp cpmvert
+	 * @return - A string with the object in json format
+	 */
+	public static String convertObjectToJson(final Gson gson, final Object jsonObject) {
+		String jsonString = null;
 
-        if (gson == null || ObjectUtils.isNull(jsonObject)) {
-            LOGGER.warning("gson or jsonObject passed in is null");
-            return jsonElement;
-        }
+		if (gson == null || ObjectUtils.isNull(jsonObject)) {
+			LOGGER.warning("gson or jsonObject passed in is null");
+			return jsonString;
+		}
 
-        jsonElement = gson.toJsonTree(jsonObject);
+		jsonString = gson.toJson(jsonObject);
+		LOGGER.fine(String.format("jsonString:%s", jsonString));
+		return jsonString;
+	}
 
-        return jsonElement;
-    }
+	/**
+	 * Method to convert an object to a json tree.
+	 *
+	 * @param gson
+	 *            gson object
+	 * @param jsonObject
+	 *            jsonObject to convert
+	 * @return a JsonElement
+	 */
+	public static JsonElement convertObjectToJsonTree(final Gson gson, final Object jsonObject) {
+		JsonElement jsonElement = null;
+
+		if (gson == null || ObjectUtils.isNull(jsonObject)) {
+			LOGGER.warning("gson or jsonObject passed in is null");
+			return jsonElement;
+		}
+
+		jsonElement = gson.toJsonTree(jsonObject);
+
+		return jsonElement;
+	}
 
 }
