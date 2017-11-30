7cea0a218b18390bbee394f0293dd92fc8134615
Damien Gallagher <damien.gallagher@gmail.com>
2017-11-30 17:39:30 +0000

    Added javadoc to new clients class

--- base/src/main/java/com/enjin/coin/sdk/service/Clients.java
+++ base/src/main/java/com/enjin/coin/sdk/service/Clients.java
@@ -2,12 +2,26 @@ package com.enjin.coin.sdk.service;
 
 import com.enjin.coin.sdk.config.Config;
 
+/**
+ * <p Method to return the necessary client. </p>
+ *
+ */
 public class Clients {
 
+    /**
+     * Method to get the sync EnjinCoint client.
+     * @param config
+     * @return
+     */
     public static EnjinCoin sync(Config config) {
         return new EnjinCoinAsyncClient(config);
     }
 
+    /**
+     * Method to get the async EnjinCoint client.
+     * @param config
+     * @return
+     */
     public static EnjinCoinAsync async(Config config) {
         return new EnjinCoinAsyncClient(config);
     }
