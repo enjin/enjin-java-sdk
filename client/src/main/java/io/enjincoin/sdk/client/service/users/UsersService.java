6ba4ac8598e87490fbe2679fa6dacdea15224cdb
Damien Gallagher <damien.gallagher@gmail.com>
2018-03-03 19:15:00 +0000

    Removed wiremock and associated tests from the project

--- client/src/main/java/io/enjincoin/sdk/client/service/users/UsersService.java
+++ client/src/main/java/io/enjincoin/sdk/client/service/users/UsersService.java
@@ -1,7 +1,4 @@
 package io.enjincoin.sdk.client.service.users;
 
-import io.enjincoin.sdk.client.service.users.AsynchronousUsersService;
-import io.enjincoin.sdk.client.service.users.SynchronousUsersService;
-
 public interface UsersService extends SynchronousUsersService, AsynchronousUsersService {
 }
