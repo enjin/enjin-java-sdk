8112bbb3662d0a8b3aa29a883398c7e11d78a310
Yukitty <catgirl@goddess.moe>
2020-04-30 00:44:41 -0400

    Made GetWallet actually work
    The current Template expects "ethAddr", not "ethAddress"
    And non-capitalization of "ethaddress" in the function name was wrong

--- sdk/src/main/java/com/enjin/sdk/models/wallet/GetWallet.java
+++ sdk/src/main/java/com/enjin/sdk/models/wallet/GetWallet.java
@@ -4,8 +4,8 @@ import com.enjin.sdk.graphql.GraphQLRequest;
 
 public class GetWallet extends GraphQLRequest<GetWallet> implements WalletFragment<GetWallet> {
 
-    public GetWallet ethaddress(String ethAddress) {
-        set("ethAddress", ethAddress);
+    public GetWallet ethAddress(String ethAddress) {
+        set("ethAddr", ethAddress);
         return this;
     }
 
