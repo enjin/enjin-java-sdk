f317750fd5f722d0255be60050bace9a831ed079
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-07-28 14:10:05 -0700

    - Added Auth, GetOne, and GetMany interfaces - Added Player and Project fragments - Added PlayerService and ProjectService - Added method to BaseService to create a retrofit service - Added PlayerService and ProjectService attributes to SchemaImpl

---
+++ sdk/src/main/java/com/enjin/sdk/schemas/Auth.java
@@ -0,0 +1,18 @@
+package com.enjin.sdk.schemas;
+
+import com.enjin.sdk.graphql.GraphQLRequest;
+import com.enjin.sdk.graphql.GraphQLResponse;
+import com.enjin.sdk.models.AccessToken;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+
+interface Auth {
+
+    @POST("graphql")
+    // TODO: Add the @GraphQuery annotation for the appropriate template, if necessary.
+    @Headers("Content-Type: application/json")
+    Call<GraphQLResponse<AccessToken>> getAuth(@Body GraphQLRequest request);
+
+}
