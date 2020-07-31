cf6af70f5908cdbc385737f099132718d1c4514a
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-07-30 17:58:41 -0700

    - Added PaginationOptions - Added PaginationArguments - GetPlayers now implements PaginationArguments

--- sdk/src/main/java/com/enjin/sdk/schemas/project/queries/GetPlayers.java
+++ sdk/src/main/java/com/enjin/sdk/schemas/project/queries/GetPlayers.java
@@ -2,14 +2,16 @@ package com.enjin.sdk.schemas.project.queries;
 
 import com.enjin.sdk.graphql.GraphQLRequest;
 import com.enjin.sdk.models.PlayerFilter;
+import com.enjin.sdk.schemas.shared.arguments.PaginationArguments;
 import com.enjin.sdk.schemas.shared.arguments.PlayerFragmentArguments;
 
 /**
  * TODO
  */
 public class GetPlayers
-        extends GraphQLRequest<GetPlayers> // TODO: Switch to PaginationRequest.
-        implements PlayerFragmentArguments<GetPlayers> {
+        extends GraphQLRequest<GetPlayers>
+        implements PaginationArguments<GetPlayers>,
+        PlayerFragmentArguments<GetPlayers> {
 
     /**
      * TODO
---
+++ sdk/src/main/java/com/enjin/sdk/schemas/shared/arguments/PaginationArguments.java
@@ -0,0 +1,34 @@
+package com.enjin.sdk.schemas.shared.arguments;
+
+import com.enjin.sdk.graphql.VariableHolder;
+import com.enjin.sdk.models.PaginationOptions;
+
+/**
+ * TODO
+ * @param <T>
+ */
+public interface PaginationArguments<T extends VariableHolder<T>> extends VariableHolder<T> {
+
+    /**
+     * TODO
+     * @param pagination
+     * @return
+     */
+    default T paginate(PaginationOptions pagination) {
+        return set("pagination", pagination);
+    }
+
+    /**
+     * TODO
+     * @param page
+     * @param limit
+     * @return
+     */
+    default T paginate(int page, int limit) {
+        PaginationOptions options = new PaginationOptions()
+                                        .page(page)
+                                        .limit(limit);
+        return set("pagination", options);
+    }
+
+}
