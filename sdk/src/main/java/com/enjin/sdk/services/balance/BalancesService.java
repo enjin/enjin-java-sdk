91dcbb136afddf22b52547d80b94010d00341f0f
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-08-10 11:55:55 -0700

    - Removed models package in preparation for refactor - Removed services package in preparation for refactor

--- sdk/src/main/java/com/enjin/sdk/services/balance/BalancesService.java
+++
@@ -1,7 +0,0 @@
-package com.enjin.sdk.services.balance;
-
-/**
- * Asynchronous and synchronous methods for querying and mutating balances.
- */
-public interface BalancesService extends SynchronousBalancesService, AsynchronousBalancesService {
-}
