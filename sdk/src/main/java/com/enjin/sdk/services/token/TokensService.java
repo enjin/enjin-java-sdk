4ff73a113e82545b9757cbd8dd55359e276087f9
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-08-10 11:55:55 -0700

    - Removed models package in preparation for refactor - Removed services package in preparation for refactor

--- sdk/src/main/java/com/enjin/sdk/services/token/TokensService.java
+++
@@ -1,9 +0,0 @@
-package com.enjin.sdk.services.token;
-
-/**
- * Asynchronous and synchronous methods for querying and mutating app tokens.
- *
- * @author Evan Lindsay
- */
-public interface TokensService extends SynchronousTokensService, AsynchronousTokensService {
-}
