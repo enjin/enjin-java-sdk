41063241f931baeee3bdd9d93d465fce206db27c
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-08-12 12:32:39 -0700

    - GetPlayer in player schema now implements PlayerFragmentArguments

--- sdk/src/main/java/com/enjin/sdk/schemas/player/queries/GetPlayer.java
+++ sdk/src/main/java/com/enjin/sdk/schemas/player/queries/GetPlayer.java
@@ -1,11 +1,14 @@
 package com.enjin.sdk.schemas.player.queries;
 
 import com.enjin.sdk.graphql.GraphQLRequest;
+import com.enjin.sdk.schemas.shared.arguments.PlayerFragmentArguments;
 
 /**
  * TODO
  */
-public class GetPlayer extends GraphQLRequest<GetPlayer> {
+public class GetPlayer
+        extends GraphQLRequest<GetPlayer>
+        implements PlayerFragmentArguments<GetPlayer> {
 
     /**
      * TODO
