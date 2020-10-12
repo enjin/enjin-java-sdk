51c961107cd97925b67779e43c361bd2320870fb
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-10-12 12:36:29 -0700

    Removed GetOne and GetMany service interfaces and transferred methods to services which previously extended them

--- sdk/src/main/java/com/enjin/sdk/services/RequestService.java
+++ sdk/src/main/java/com/enjin/sdk/services/RequestService.java
@@ -1,9 +1,29 @@
 package com.enjin.sdk.services;
 
+import com.enjin.sdk.graphql.GraphQLResponse;
 import com.enjin.sdk.models.Request;
+import com.google.gson.JsonObject;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+import java.util.List;
 
 /**
  * Used internally for transaction requests.
  */
-public interface RequestService extends GetOne<Request>, GetMany<Request> {
+public interface RequestService {
+
+    @POST("/graphql/{schema}")
+    @Headers("Content-Type: application/json")
+    Call<GraphQLResponse<Request>> getOne(@Path("schema") String schema,
+                                          @Body JsonObject request);
+
+    @POST("/graphql/{schema}")
+    @Headers("Content-Type: application/json")
+    Call<GraphQLResponse<List<Request>>> getMany(@Path("schema") String schema,
+                                                 @Body JsonObject request);
+
 }
