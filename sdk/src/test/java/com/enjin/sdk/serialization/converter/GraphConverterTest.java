dd4ae72ab5c203473466d28a29d8b8cb8aa139f1
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-10-19 15:32:57 -0700

    Added unit tests for GraphConverter

---
+++ sdk/src/test/java/com/enjin/sdk/serialization/converter/GraphConverterTest.java
@@ -0,0 +1,278 @@
+package com.enjin.sdk.serialization.converter;
+
+import com.enjin.sdk.graphql.GraphQLError;
+import com.enjin.sdk.graphql.GraphQLResponse;
+import com.enjin.sdk.models.PaginationCursor;
+import com.enjin.sdk.serialization.converter.GraphConverter.GraphResponseConverter;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import lombok.EqualsAndHashCode;
+import lombok.SneakyThrows;
+import okhttp3.ResponseBody;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import retrofit2.Converter;
+import retrofit2.Retrofit;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+class GraphConverterTest {
+
+    private static final Gson GSON = new GsonBuilder()
+            .setLenient()
+            .create();
+    private static final DummyObject RESULT_ONE = DummyObject.createDefault();
+    private static final List<DummyObject> RESULT_MANY = new ArrayList<>();
+    private static final List<GraphQLError> ERRORS = new ArrayList<>();
+    private static final PaginationCursor CURSOR = new PaginationCursor();
+    private static final MockWebServer MOCK_WEB_SERVER = new MockWebServer();
+
+    private final GraphConverter classUnderTest = new GraphConverter();
+
+    @BeforeAll
+    @SneakyThrows
+    public static void BeforeAll() {
+        RESULT_MANY.add(new DummyObject(1));
+        RESULT_MANY.add(new DummyObject(2));
+        RESULT_MANY.add(new DummyObject(3));
+        ERRORS.add(new GraphQLError());
+        ERRORS.add(new GraphQLError());
+        ERRORS.add(new GraphQLError());
+        MOCK_WEB_SERVER.start();
+    }
+
+    @AfterAll
+    @SneakyThrows
+    public static void AfterAll() {
+        MOCK_WEB_SERVER.shutdown();
+    }
+
+    @Test
+    void responseBodyConverter_TypeIsParameterizedType_RawTypeIsGraphQLResponse_ReturnsGraphResponseConverter() {
+        // Arrange - Data
+        final ParameterizedType mockType = mock(ParameterizedType.class);
+        final Annotation[] dummyAnnotations = createDummyAnnotations();
+        final Retrofit dummyRetrofit = createDummyRetrofit();
+
+        // Arrange - Stubbing
+        when(mockType.getRawType()).thenReturn(GraphQLResponse.class);
+        when(mockType.getActualTypeArguments()).thenReturn(new Type[] {Object.class});
+
+        // Act
+        Converter<ResponseBody, ?> actual = classUnderTest.responseBodyConverter(mockType,
+                                                                                 dummyAnnotations,
+                                                                                 dummyRetrofit);
+
+        // Verify
+        verify(mockType)
+                .getRawType();
+        verify(mockType)
+                .getActualTypeArguments();
+
+        // Assert
+        assertNotNull(actual);
+    }
+
+    @Test
+    void responseBodyConverter_TypeIsParameterizedType_RawTypeIsNotGraphQLResponse_ReturnNull() {
+        // Arrange - Data
+        final ParameterizedType mockType = mock(ParameterizedType.class);
+        final Annotation[] dummyAnnotations = createDummyAnnotations();
+        final Retrofit dummyRetrofit = createDummyRetrofit();
+
+        // Arrange - Stubbing
+        when(mockType.getRawType()).thenReturn(Object.class);
+
+        // Act
+        Converter<ResponseBody, ?> actual = classUnderTest.responseBodyConverter(mockType,
+                                                                                 dummyAnnotations,
+                                                                                 dummyRetrofit);
+
+        // Verify
+        verify(mockType)
+                .getRawType();
+
+        // Assert
+        assertNull(actual);
+    }
+
+    @Test
+    void responseBodyConverter_TypeIsNotParameterizedType_ReturnNull() {
+        // Arrange
+        final Type mockType = mock(Type.class);
+        final Annotation[] dummyAnnotations = createDummyAnnotations();
+        final Retrofit dummyRetrofit = createDummyRetrofit();
+
+        // Act
+        Converter<ResponseBody, ?> actual = classUnderTest.responseBodyConverter(mockType,
+                                                                                 dummyAnnotations,
+                                                                                 dummyRetrofit);
+
+        // Assert
+        assertNull(actual);
+    }
+
+    @Test
+    void create_ReturnsGraphConverter() {
+        // Act
+        GraphConverter actual = GraphConverter.create();
+
+        // Assert
+        assertNotNull(actual);
+    }
+
+    @Test
+    @SneakyThrows
+    void convert_SingleDataInRaw_ResponseHasData() {
+        // Arrange - Data
+        final DummyObject expectedData = RESULT_ONE;
+        final String expectedRaw = String.format("{\"data\": {\"result\": %s}}", GSON.toJson(expectedData));
+        final Type responseType = DummyObject.class;
+        final GraphResponseConverter<DummyObject> responseConverter = createGraphResponseConverter(classUnderTest,
+                                                                                                   responseType);
+        final ResponseBody mockResponseBody = mock(ResponseBody.class);
+
+        // Arrange - Stubbing
+        when(mockResponseBody.string()).thenReturn(expectedRaw);
+
+        // Act
+        GraphQLResponse<DummyObject> actual = responseConverter.convert(mockResponseBody);
+
+        // Verify
+        verify(mockResponseBody)
+                .string();
+
+        // Assert
+        assertNotNull(actual);
+        assertEquals(expectedRaw, actual.getRaw());
+        assertEquals(expectedData, actual.getData());
+    }
+
+    @Test
+    @SneakyThrows
+    void convert_MultipleDataInRaw_ResponseHasData() {
+        // Arrange - Data
+        final List<DummyObject> expectedData = RESULT_MANY;
+        final String expectedRaw = String.format("{\"data\": {\"result\": %s}}", GSON.toJson(expectedData));
+        final Type responseType = TypeToken.getParameterized(List.class, DummyObject.class).getType();
+        final GraphResponseConverter<List<DummyObject>> responseConverter = createGraphResponseConverter(classUnderTest,
+                                                                                                         responseType);
+        final ResponseBody mockResponseBody = mock(ResponseBody.class);
+
+        // Arrange - Stubbing
+        when(mockResponseBody.string()).thenReturn(expectedRaw);
+
+        // Act
+        GraphQLResponse<List<DummyObject>> actual = responseConverter.convert(mockResponseBody);
+
+        // Verify
+        verify(mockResponseBody)
+                .string();
+
+        // Assert
+        assertNotNull(actual);
+        assertEquals(expectedRaw, actual.getRaw());
+        assertEquals(expectedData, actual.getData());
+    }
+
+    @Test
+    @SneakyThrows
+    void convert_ErrorsInRaw_ResponseHasErrors() {
+        // Arrange - Data
+        final List<GraphQLError> expectedErrors = ERRORS;
+        final String expectedRaw = String.format("{\"errors\": %s}", GSON.toJson(expectedErrors));
+        final Type responseType = DummyObject.class;
+        final GraphResponseConverter<DummyObject> responseConverter = createGraphResponseConverter(classUnderTest,
+                                                                                                   responseType);
+        final ResponseBody mockResponseBody = mock(ResponseBody.class);
+
+        // Arrange - Stubbing
+        when(mockResponseBody.string()).thenReturn(expectedRaw);
+
+        // Act
+        GraphQLResponse<DummyObject> actual = responseConverter.convert(mockResponseBody);
+
+        // Verify
+        verify(mockResponseBody)
+                .string();
+
+        // Assert
+        assertNotNull(actual);
+        assertEquals(expectedRaw, actual.getRaw());
+        assertEquals(expectedErrors, actual.getErrors());
+    }
+
+    @Test
+    @SneakyThrows
+    void convert_CursorInRaw_ResponseHasCursor() {
+        // Arrange - Data
+        final PaginationCursor expectedCursor = CURSOR;
+        final String expectedRaw = String.format("{\"data\": {\"result\": {\"cursor\": %s}}}",
+                                                 GSON.toJson(expectedCursor));
+        final Type responseType = DummyObject.class;
+        final GraphResponseConverter<DummyObject> responseConverter = createGraphResponseConverter(classUnderTest,
+                                                                                                   responseType);
+        final ResponseBody mockResponseBody = mock(ResponseBody.class);
+
+        // Arrange - Stubbing
+        when(mockResponseBody.string()).thenReturn(expectedRaw);
+
+        // Act
+        GraphQLResponse<DummyObject> actual = responseConverter.convert(mockResponseBody);
+
+        // Verify
+        verify(mockResponseBody)
+                .string();
+
+        // Assert
+        assertNotNull(actual);
+        assertEquals(expectedRaw, actual.getRaw());
+        assertEquals(expectedCursor, actual.getCursor());
+    }
+
+    private static <T> GraphResponseConverter<T> createGraphResponseConverter(GraphConverter converter, Type... types) {
+        ParameterizedType stubType = mock(ParameterizedType.class);
+        when(stubType.getRawType()).thenReturn(GraphQLResponse.class);
+        when(stubType.getActualTypeArguments()).thenReturn(types);
+        return (GraphResponseConverter<T>) converter.responseBodyConverter(stubType,
+                                                                           createDummyAnnotations(),
+                                                                           createDummyRetrofit());
+    }
+
+    private static Annotation[] createDummyAnnotations() {
+        return new Annotation[0];
+    }
+
+    private static Retrofit createDummyRetrofit() {
+        return new Retrofit.Builder()
+                .baseUrl(MOCK_WEB_SERVER.url("/"))
+                .build();
+    }
+
+    @EqualsAndHashCode
+    private static class DummyObject {
+
+        private final Integer integer;
+
+        public DummyObject(Integer integer) {
+            this.integer = integer;
+        }
+
+        public static DummyObject createDefault() {
+            return new DummyObject(1);
+        }
+
+    }
+
+}
