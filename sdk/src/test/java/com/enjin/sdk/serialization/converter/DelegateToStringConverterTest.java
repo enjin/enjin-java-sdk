9cd1c5822ef00644786015be99e524156a7b9523
BishopJohnson <bishop.s.johnson.wa@gmail.com>
2020-10-20 14:43:00 -0700

    - Added unit tests for JsonStringConverter

---
+++ sdk/src/test/java/com/enjin/sdk/serialization/converter/DelegateToStringConverterTest.java
@@ -0,0 +1,49 @@
+package com.enjin.sdk.serialization.converter;
+
+import com.enjin.sdk.serialization.converter.JsonStringConverter.DelegateToStringConverter;
+import lombok.SneakyThrows;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.junit.jupiter.MockitoExtension;
+import retrofit2.Converter;
+
+@ExtendWith(MockitoExtension.class)
+class DelegateToStringConverterTest {
+
+    private static final MediaType MEDIA_TYPE = MediaType.parse("application/json");
+
+    @Mock(name = "delegate")
+    private Converter<Object, RequestBody> mockConverter;
+
+    @InjectMocks
+    private DelegateToStringConverter<Object> classUnderTest;
+
+    @Test
+    @SneakyThrows
+    void convert_ReturnsNonNullString() {
+        // Arrange - Data
+        final Object dummyObject = new Object();
+        final RequestBody fakeResponseBody = RequestBody.create("", MEDIA_TYPE);
+
+        // Arrange - Stubbing
+        when(mockConverter.convert(dummyObject)).thenReturn(fakeResponseBody);
+
+        // Act
+        String actual = classUnderTest.convert(dummyObject);
+
+        // Verify
+        verify(mockConverter)
+                .convert(dummyObject);
+
+        // Assert
+        assertNotNull(actual);
+    }
+
+}
---
+++ sdk/src/test/java/com/enjin/sdk/serialization/converter/JsonStringConverterTest.java
@@ -0,0 +1,99 @@
+package com.enjin.sdk.serialization.converter;
+
+import com.enjin.sdk.serialization.Json;
+import com.enjin.sdk.serialization.converter.JsonStringConverter.DelegateToStringConverter;
+import lombok.SneakyThrows;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterAll;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import retrofit2.Converter;
+import retrofit2.Retrofit;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+class JsonStringConverterTest {
+
+    private static final MockWebServer MOCK_WEB_SERVER = new MockWebServer();
+
+    @BeforeAll
+    @SneakyThrows
+    public static void BeforeAll() {
+        MOCK_WEB_SERVER.start();
+    }
+
+    @AfterAll
+    @SneakyThrows
+    public static void AfterAll() {
+        MOCK_WEB_SERVER.shutdown();
+    }
+
+    @Test
+    void create_ReturnsJsonStringConverter() {
+        // Arrange
+        final Converter.Factory dummyFactory = mock(Converter.Factory.class);
+
+        // Act
+        JsonStringConverter actual = JsonStringConverter.create(dummyFactory);
+
+        // Assert
+        assertNotNull(actual);
+    }
+
+    @Test
+    void stringConverter_AnnotationsDoesNotContainJsonAnnotation_ReturnsNull() {
+        // Arrange
+        final Converter.Factory dummyFactory = mock(Converter.Factory.class);
+        final Type dummyType = mock(Type.class);
+        final Retrofit dummyRetrofit = createDummyRetrofit();
+        final Annotation[] fakeAnnotations = new Annotation[0];
+        final JsonStringConverter converter = new JsonStringConverter(dummyFactory);
+
+        // Act
+        DelegateToStringConverter actual = (DelegateToStringConverter) converter.stringConverter(dummyType,
+                                                                                                 fakeAnnotations,
+                                                                                                 dummyRetrofit);
+
+        // Assert
+        assertNull(actual);
+    }
+
+    @Test
+    void stringConverter_AnnotationsContainsJsonAnnotation_ReturnsDelegateToStringConverter() {
+        // Arrange - Data
+        final Type dummyType = mock(Type.class);
+        final Retrofit dummyRetrofit = createDummyRetrofit();
+        final Converter dummyRequestBodyConverter = mock(Converter.class);
+        final Annotation[] fakeAnnotations = new Annotation[] {mock(Json.class)};
+        final Converter.Factory mockFactory = mock(Converter.Factory.class);
+        final JsonStringConverter converter = new JsonStringConverter(mockFactory);
+
+        // Arrange - Stubbing
+        when(mockFactory.requestBodyConverter(any(), any(), any(), any())).thenReturn(dummyRequestBodyConverter);
+
+        // Act
+        DelegateToStringConverter actual = (DelegateToStringConverter) converter.stringConverter(dummyType,
+                                                                                                 fakeAnnotations,
+                                                                                                 dummyRetrofit);
+
+        // Verify
+        verify(mockFactory)
+                .requestBodyConverter(any(), any(), any(), any());
+
+        // Assert
+        assertNotNull(actual);
+    }
+
+    private static Retrofit createDummyRetrofit() {
+        return new Retrofit.Builder()
+                .baseUrl(MOCK_WEB_SERVER.url("/"))
+                .build();
+    }
+
+}
