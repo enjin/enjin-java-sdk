1e00bdc783b88510d6824a8e390d7f6129417739
BishopJohnson <bishop@enjin.io>
2021-04-27 16:15:12 -0700

    Added ILogger interface and LogLevel enums

---
+++ sdk/src/main/java/com/enjin/sdk/utils/LogLevel.java
@@ -0,0 +1,13 @@
+package com.enjin.sdk.utils;
+
+/**
+ * Enum values representing the different logging severity levels.
+ */
+public enum LogLevel {
+    TRACE,
+    DEBUG,
+    INFO,
+    WARN,
+    ERROR,
+    SEVERE,
+}
