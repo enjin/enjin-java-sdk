72f88daa7b62d7e5f5dc5f638e21c2ef5cb9deab
Bishop S Johnson <bishop@enjin.io>
2021-12-09 18:00:39 -0800

    Added temporary always failing test to test workflow

--- sdk/src/test/java/com/enjin/sdk/PlayerClientTest.java
+++ sdk/src/test/java/com/enjin/sdk/PlayerClientTest.java
@@ -45,6 +45,12 @@ class PlayerClientTest {
         mockWebServer.shutdown();
     }
 
+    // TODO: DELETE THIS AFTER TESTING WORKFLOW ON GITHUB.
+    @Test
+    void failedTest() {
+        fail();
+    }
+
     @Test
     void isAuthenticated_AuthenticatedWithValidToken_ReturnsTrue() {
         // Arrange
