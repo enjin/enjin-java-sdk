eaf61542d2ac7e689b4989ec46e9906f2baf0f36
Bishop Johnson <BishopJohnson@users.noreply.github.com>
2023-02-13 13:13:19 -0800

    [DX 305] Add `GetAssetsFromProjects` query (#109)
    - Added GetAssetsFromProjects.gql query template
    - Added `GetAssetsFromProjects` query to project schema

---
+++ sdk/src/main/java/com/enjin/sdk/schemas/project/queries/GetAssetsFromProjects.java
@@ -0,0 +1,74 @@
+// Copyright 2023 Enjin Pte. Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.enjin.sdk.schemas.project.queries;
+
+import com.enjin.sdk.graphql.GraphQLRequest;
+import com.enjin.sdk.models.AssetFilter;
+import com.enjin.sdk.models.AssetSortInput;
+import com.enjin.sdk.schemas.shared.arguments.AssetFragmentArguments;
+import com.enjin.sdk.schemas.shared.arguments.PaginationArguments;
+
+/**
+ * Request for getting assets from different projects on the platform.
+ *
+ * @see com.enjin.sdk.models.Asset
+ * @see com.enjin.sdk.schemas.project.IProjectSchema
+ */
+public class GetAssetsFromProjects
+        extends GraphQLRequest<GetAssetsFromProjects>
+        implements AssetFragmentArguments<GetAssetsFromProjects>,
+        PaginationArguments<GetAssetsFromProjects> {
+
+    /**
+     * Sole constructor.
+     */
+    public GetAssetsFromProjects() {
+        super("enjin.sdk.project.GetAssetsFromProjects");
+    }
+
+    /**
+     * Sets the project UUIDs.
+     *
+     * @param projects the UUIDs
+     *
+     * @return this request for chaining
+     */
+    public GetAssetsFromProjects projects(String... projects) {
+        return set("projects", projects);
+    }
+
+    /**
+     * Sets the filter the request will use.
+     *
+     * @param filter the filter
+     *
+     * @return this request for chaining
+     */
+    public GetAssetsFromProjects filter(AssetFilter filter) {
+        return set("filter", filter);
+    }
+
+    /**
+     * Sets the request to sort the results by the specified options.
+     *
+     * @param sort the sort input
+     *
+     * @return this request for chaining
+     */
+    public GetAssetsFromProjects sort(AssetSortInput sort) {
+        return set("sort", sort);
+    }
+
+}
